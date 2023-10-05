ae5ae27fb003961b68a6567af4fecaab1cdd8530
Pooja Bambal <bambalpooja23@gmail.com>
2023-10-05 08:44:37 +0530

    initial-setup

---
+++ expenseitem/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import ExpenseItem from './Components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
